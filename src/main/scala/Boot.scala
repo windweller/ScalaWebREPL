38990a60fdb79dd73b3e5b03957a0e7cbbada57f
windwller <leo.niecn@gmail.com>
2014-08-30 13:25:25 -0400

    Initial Commit

--- src/main/scala/Boot.scala
+++ src/main/scala/Boot.scala
@@ -1,17 +1,20 @@
-import java.io.File
 
 import akka.actor.{Props, ActorSystem}
 import akka.io.IO
 import spray.can.Http
-import spray.http.MediaTypes._
-import spray.routing._
 
 object Boot extends App {
   implicit lazy val system = ActorSystem("spray-blog")
 
-  val host = "localhost"
-  val portHTTP = 8080
-  val portWs = 6696
+  import com.typesafe.config.ConfigFactory
+
+  private val config = ConfigFactory.load
+  config.checkValid(ConfigFactory.defaultReference)
+
+  val host = config.getString("service.host")
+  val portHTTP = config.getInt("service.port")
+  val portTcp = config.getInt("service.ports.tcp")
+  val portWs = config.getInt("service.ports.ws")
 
   private val ws = new WsServer(portWs)
   val processActor = system.actorOf(Props(new ProcessActor))
@@ -24,3 +27,4 @@ object Boot extends App {
 
   IO(Http) ! Http.Bind(rootService, interface = host, port = portHTTP)
 }
+
